ebeb84061d9aec2a6d1748d769539b8eb987c5a4
Hüseyin VURAL <huseyin@meditopia.com>
2023-02-24 23:30:05 +0300

    Add custom init.

--- Sources/TestLib/TestXib.swift
+++ Sources/TestLib/TestXib.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 public class TestXib: UIViewController {
-
+    
+    public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: "TestXib", bundle: Bundle.module)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
 
