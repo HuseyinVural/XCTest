6ae47c89d6a40d5a7c1d5c398a0c4dc466c5a1b6
Hüseyin VURAL <huseyin@meditopia.com>
2023-02-24 23:16:58 +0300

    Change access controller.

--- Sources/TestLib/TestXib.swift
+++ Sources/TestLib/TestXib.swift
@@ -7,9 +7,9 @@
 
 import UIKit
 
-class TestXib: UIViewController {
+public class TestXib: UIViewController {
 
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
